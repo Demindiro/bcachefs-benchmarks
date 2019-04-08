68b62ad11f30f8ebb3368b27f01a2e9592777037
David Hoppenbrouwers <david@demindiro.com>
2019-04-08 21:25:24 +0200

    Generated all consistency check stuff
    I think? Yes I did.

--- format-allcol.sh
+++ format-allcol.sh
@@ -4,16 +4,20 @@ PLOTDIR=$1
 NAME=$2
 FILES=${@:3}
 
-./format-results.sh  3 $FILES > data/$PLOTDIR/$NAME-write.tsv
-./format-results.sh  4 $FILES > data/$PLOTDIR/$NAME-rewrite.tsv
-./format-results.sh  5 $FILES > data/$PLOTDIR/$NAME-read.tsv
-./format-results.sh  6 $FILES > data/$PLOTDIR/$NAME-reread.tsv
-./format-results.sh  7 $FILES > data/$PLOTDIR/$NAME-random-read.tsv
-./format-results.sh  8 $FILES > data/$PLOTDIR/$NAME-random-write.tsv
-./format-results.sh  9 $FILES > data/$PLOTDIR/$NAME-bkwd-read.tsv
-./format-results.sh 10 $FILES > data/$PLOTDIR/$NAME-record-rewrite.tsv
-./format-results.sh 11 $FILES > data/$PLOTDIR/$NAME-stride-read.tsv
-./format-results.sh 12 $FILES > data/$PLOTDIR/$NAME-fwrite.tsv
-./format-results.sh 13 $FILES > data/$PLOTDIR/$NAME-frewrite.tsv
-./format-results.sh 14 $FILES > data/$PLOTDIR/$NAME-fread.tsv
-./format-results.sh 15 $FILES > data/$PLOTDIR/$NAME-freread.tsv
+function f() {
+	echo data/$PLOTDIR/$NAME-$2
+	./format-results.sh  $1 $FILES > data/$PLOTDIR/$NAME-$2.tsv
+}
+f  3 write
+f  4 rewrite
+f  5 read
+f  6 reread
+f  7 random-read
+f  8 random-write
+f  9 bkwd-read
+f 10 record-rewrite
+f 11 stride-read
+f 12 fwrite
+f 13 frewrite
+f 14 fread
+f 15 freread
--- mkplot.sh
+++ mkplot.sh
@@ -3,7 +3,7 @@
 for f in ${@:1}; do
 	for t in $(echo write rewrite read reread random-read random-write bkwd-read record-rewrite stride-read fwrite frewrite fread freread); do 
 		FILE=$f-$t
-		echo $FILE
+		echo plots/$FILE
 		gnuplot -p histogram.gnu < data/$FILE.tsv > plots/$FILE.png
 	done
 done
